e94413ac3ebb7c93701026338b5faa31e25e4565
Khushi Kothari <62203565+khushi-kothari@users.noreply.github.com>
2024-01-09 23:58:28 +0530

    Add pagination

--- src/components/pages/Issues.jsx
+++ src/components/pages/Issues.jsx
@@ -4,27 +4,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from 'axios';
 import List from '../sub-components/List'
 import { convertTime } from '../sub-components/TimeStamp';
+import { isDark } from '../sub-components/isDark';
 
 
 function Issues() {
     const [results, setResults] = useState([])
     const [callFetch, setCallFetch] = useState(false);
+    const [urls, setUrls] = useState();
+    const [url, setUrl] = useState('https://api.github.com/search/issues?q=label:goodfirstissue&sort:updatedat')
 
     useEffect(() => {
         if (callFetch) {
             const search = async () => {
                 try {
                     const accessToken = import.meta.env.ACCESS_TOKEN;
-                    let apiUrl = `https://api.github.com/search/issues?q=label:goodfirstissue`;
+                    let apiUrl = { url };
                     const response = await axios.get(apiUrl, {
                         headers: {
                             'Authorization': `${accessToken}`,
                         },
                     });
+
+                    const link = response.headers.get("link");
+                    const links = link.split(",");
+                    const urls = links.map(a => {
+                        return {
+                            url: a.split(';')[0].replace(">", "").replace("<", ""),
+                            title: a.split(';')[1].substring(6, 10)
+                        }
+                    })
+                    setUrls(urls);
+                    console.log('links', urls)
+
+
                     const data = response.data.items;
                     setResults(data);
                     console.log("data items: ", data, "type : ", Array.isArray(data));
-                    console.log("Link in the header", response.headers.get("link"));
+                    // console.log("Link in the header", response.headers.get("link"));
                 } catch (error) {
                     console.error('Error:', error);
                     setResults([]);
@@ -50,14 +66,14 @@ function Issues() {
                                         {/* backgroundColor : #${l[i].color};  */}
                                         <button
                                             style={{ 'backgroundColor': `#${l.color}`, }}
-                                            className='bg-opacity-95'
+                                            className={`${isDark(l.color) ? "text-white " : "text-black "}bg-opacity-95 mr-4 p-2 my-2 rounded-md`}
                                         >{l.name}</button>
                                     </React.Fragment>
                                 ))
                             }
                             <br></br>
                             <p>{r.state}</p>
-                            <a href={r.url} className='underline text-blue-600'>Go to issue</a>
+                            <a href={r.url} className='underline text-blue-600'>Go to issue <sup>↗</sup></a>
                             <div className='flex'>
                                 <img src={r.user.avatar_url}
                                     className='h-8 w-8 rounded-full mr-2' />
@@ -80,6 +96,12 @@ function Issues() {
             >
                 <FontAwesomeIcon icon="fa-solid fa-arrows-rotate" className='px-2' />
                 Refresh</div>
+
+            {urls.map((u) => {
+                <button
+                    onClick={(e) => setUrl(u.url)}
+                >{u.title}</button>
+            })}
         </>
     )
 }
