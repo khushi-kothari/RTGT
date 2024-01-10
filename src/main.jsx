740b345eed88b8bbd09e5d55d5f55b4983a01187
Khushi Kothari <62203565+khushi-kothari@users.noreply.github.com>
2024-01-10 21:41:22 +0530

    Update Routes rules to solve vercel prod mode error

--- src/main.jsx
+++ src/main.jsx
@@ -8,19 +8,9 @@ import {
 } from "react-router-dom";
 import Issues from './components/pages/Issues.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: '/issue-search',
-    element: <Issues />
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <RouterProvider router={router} />
+    <App />
   </>,
 )
